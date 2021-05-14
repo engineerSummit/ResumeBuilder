e4a08e86eb0e5c263bedfa4c2ea1a7b9858508b8
Shivam16Gupta <shivamgupta1281@gmail.com>
2021-05-14 21:03:21 +0530

    added scss

--- resume/src/components/template.jsx
+++ resume/src/components/template.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../assets/scss/template.min.css';
 
 const Template =()=>{
   return(
     <>
+    <div class="wrapper">
     <div class="container-R">
     <div class="template-L">
     </div>
     <div class="template-R">
+    </div>
     </div>
     </div>
     </>
