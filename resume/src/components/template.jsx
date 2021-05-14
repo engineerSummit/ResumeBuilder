fbb3832f43b13db4ed7b7e9873cdf6b7ed723597
Shivam16Gupta <shivamgupta1281@gmail.com>
2021-05-14 20:19:24 +0530

    added folders

---
+++ resume/src/components/template.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Template =()=>{
+  return(
+    <>
+    <div class="container-R">
+    <div class="template-L">
+    </div>
+    <div class="template-R">
+    </div>
+    </div>
+    </>
+  );
+};
+export default Template;
