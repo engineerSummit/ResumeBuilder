c53d0f8c827c635ca1a66ab199f3c7d473e594e6
Shivam16Gupta <shivamgupta1281@gmail.com>
2021-05-19 21:56:25 +0530

    added page in container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 const Container = () => {
     return (
         <div className="container-R">
+          <div class="page">
+
             <div class="template-L">
+
             </div>
             <div class="template-R">
+
             </div>
+          </div>
         </div>
     )
 }
