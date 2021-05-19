c0691bde0c996fcdcb71ebc1fbf167ac988450ec
anas1503 <md.anas1503@gmail.com>
2021-05-19 16:23:12 +0530

    add mode handling functionality

---
+++ src/components/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Container = () => {
+    return (
+        <div className="container-R">
+            <div class="template-L">
+            </div>
+            <div class="template-R">
+            </div>
+        </div>
+    )
+}
+
+export default Container
--- src/components/template.jsx
+++ src/components/template.jsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import Footer from './Footer'
 import Buttons from './Buttons'
+import Container from './Container'
 import logo1 from '../assets/images/Mask Group.png'
 import mode from '../assets/images/mode.svg'
 import Rside from '../assets/images/Lside.svg'
 import Lside from '../assets/images/Rside.svg'
+import Sicon from '../assets/images/smallicon.png'
 
-const Template =()=>{
-  return(
+const Template = () => {
+
+  let n = 0;
+  const modehandler = () => {
+
+    n++;
+
+    if (n % 2 === 0) {
+      document.getElementById("main").style.backgroundColor = "#393e46";
+      document.getElementById("modes").title = "LIGHT MODE";
+    }
+
+    else {
+      document.getElementById("main").style.backgroundColor = "white";
+      document.getElementById("modes").title = "DARK MODE";
+    }
+  }
+
+
+  return (
     <>
-    <div class="wrapper">
-    <p className="quote">Build A Resume That Inspires</p>
-    <img className="logo"  src={logo1} alt="OUR LOGO" />
-    <img className="mode"  src={mode} alt="MODE ICON"  />
-    <img className="Rside" src={Rside} alt="illustration"  />
-    <img className="Lside" src={Lside} alt="illustration"  />
-    <div class="container-R">
-    <div class="template-L">
-    </div>
-    <div class="template-R">
-    </div>
-    </div>
-    <Buttons />
-    <Footer />
+      <div class="wrapper" id="main">
+        <p className="quote">Build A Resume That Inspires</p>
+        <img className="logo" src={logo1} alt="OUR LOGO" />
+        <img onClick={modehandler} title="LIGHT MODE" className="mode" id="modes" src={mode} alt="MODE ICON" />
+        <img className="Rside" src={Rside} alt="illustration" />
+        <img className="Lside" src={Lside} alt="illustration" />
+        {/* <img className="icons" src={Sicon} alt="icons" /> */}
 
-    </div>
+        <Container />
+
+        <Buttons />
+        <Footer />
+
+      </div>
     </>
   );
 };
