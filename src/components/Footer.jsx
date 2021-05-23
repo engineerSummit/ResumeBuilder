b1c9a621925914b4451bfb299279b3461d5d0a74
anas1503 <md.anas1503@gmail.com>
2021-05-23 17:22:52 +0530

    added footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Icons from './Icons'
 
 const Footer = () => {
     return (
         <div className="footer">
-            
+            <div className="ft-item1">
+                <p style={{ color: " #393E46", fontWeight: "bold" }}>Learn</p>
+                <p>Apply</p>
+            </div>
+
+            <div className="ft-item2">
+                <p style={{ color: "#393E46", fontWeight: "bold" }}>Join us</p>
+                <p>Mentor</p>
+                <p>Student</p>
+            </div>
+
+            <div className="ft-item3">
+                <p style={{ color: " #393E46", fontWeight: "bold" }}>Contact us</p>
+                <a href="officialengineersummit@gmail.com" target="blank">officialengineersummit@gmail.com</a>
+                <p>New delhi</p>
+            </div>
+            <p className="line"></p>
+            <p className="copy"><span style={{ fontSize: "18px" }}>&#169;</span> 2021</p>
+            <Icons />
         </div>
     )
 }
---
+++ src/components/Icons.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const Icons = () => {
+    return (
+        <div className="icons">
+            <a href="https://www.youtube.com/channel/UCXuy8rldhATY0qxxvwglWbw"><YouTubeIcon style={{ color: "#FF0000" }} /></a>
+
+            <a href="https://t.me/engineersummit"><TelegramIcon style={{ color: "#0088cc" }} /></a>
+
+            <a href="https://www.instagram.com/engineer_summit/?igshid=nunf8nbv7agj"><InstagramIcon style={{ color: "red" }} /></a>
+
+            <a href="https://www.linkedin.com/in/engineersummit-b20a02202/"><LinkedInIcon style={{ color: "#0e76a8" }} /></a>
+            
+            <a href="https://github.com/engineerSummit"><GitHubIcon style={{ color: "black" }} /></a>
+        </div>
+    )
+}
+
+export default Icons
